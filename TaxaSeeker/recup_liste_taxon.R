60bd2af5fe1fd21c5d4d2c4654c9df8741b1af98
PaulineSGN <107857379+PaulineSGN@users.noreply.github.com>
2023-07-25 13:30:08 +0200

    Add files via upload

--- TaxaSeeker/recup_liste_taxon.R
+++ TaxaSeeker/recup_liste_taxon.R
@@ -2,10 +2,9 @@
 #Ainsi que la liste des taxon 
 
 #load packages
-library(dplyr)
-library(taxonomyCleanr)
-library(stringr)
-library(xlsx)
+library(dplyr, warn.conflicts = FALSE)
+library(taxonomyCleanr, warn.conflicts = FALSE)
+library(stringr, warn.conflicts = FALSE)
  
 #load arguments
 args = commandArgs(trailingOnly=TRUE) 
@@ -41,7 +40,7 @@ have.model <- function(taxon_phylum,noms_sp,comptage_sp,brt_phylum){
       brt = c(tax,"No", pres)
       have_model = rbind(have_model,brt, make.row.names = F)}
   }
-  colnames(have_model) = c("taxon","Model","nb_presence")
+  colnames(have_model) = c("Taxa","Model","Occurences")
   return(have_model)}
 
 ##########Execution########
@@ -73,23 +72,28 @@ have_model3 = subset(have_model, have_model$`Model` != "N")
 
 #Obtention d'une liste de taxon (nettoyé) ayant obtenu un modèle BRT (fichier qui sera soumis à l'outils match taxa de la base de données WoRMS pour obtenir leur classification et pouvoir trier les doublons entre les rangs taxonomique)
 
-have_model2$taxon <- as.character(trim_taxa(have_model2$taxon))
+have_model2$Taxa <- as.character(trim_taxa(have_model2$Taxa))
 
 #Second nettoyage (élimination de tout les taxon qui finissent par sp1./sp2 etc qui represente un doublon)
 
-have_model2 <- have_model2 %>% filter(!str_ends(taxon, "sp.1|sp[0-9]"))
-have_model3 <- have_model3 %>% filter(!str_ends(taxon, "sp.1|sp[0-9]"))
-have_model <- have_model %>% filter(!str_ends(taxon, "sp.1|sp[0-9]"))
+have_model2 <- have_model2 %>% filter(!str_ends(Taxa, "sp.1|sp[0-9]"))
+have_model3 <- have_model3 %>% filter(!str_ends(Taxa, "sp.1|sp[0-9]"))
+have_model <- have_model %>% filter(!str_ends(Taxa, "sp.1|sp[0-9]"))
 
 #extraction de l'objet have_model
-write.csv(have_model,file = "have_model.csv", quote = F, row.names = F, col.names = F)
+write.csv(have_model,file = "have_model.csv", quote = F, row.names = F)
 
 #obtention de la liste pour la suite du workflow si on n'utilise pas worms
-list_taxon = have_model3$taxon
+list_taxon = have_model3$Taxa
 write.table(list_taxon, file= "list_taxa.txt", quote = F, row.names = F, col.names = F)
 
 #obtention de la liste finale à soumettre a worms
-liste_taxon = have_model2$taxon
+liste_taxon = have_model2$Taxa
 write.table(liste_taxon,file = "list_taxa_clean.txt", quote = F, row.names = F, col.names = F)
 
+
+
+
+
+#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
 
